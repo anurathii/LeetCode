326a1cbd367d687f1935812569624de777452985
Anurathi L <l.anurathi26@gmail.com>
2026-07-31 11:58:00 +0530

    Time: 2378 ms (5.13%) | Memory: 20.7 MB (35.42%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            count = 0
+            for j in  range(n):
+                if nums[j] == nums[i]:
+                    count += 1
+            if count == 1:
+                return nums[i]
+
+                    
+        
